103df569f8719605517cb5d924782c8d55e4f267
BELGUESMI-Henda <77270487+BELGUESMI-Henda@users.noreply.github.com>
2021-04-08 10:31:13 -0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Homepage from './components/Homepage/Homepage'
+import RouteSelection from './components/RouteSelection/RouteSelection'
+import LogOrsign from './components/Login-Signup/LogOrsign'
+import Signup from './components/Login-Signup/Signup'
+import Profile from './components/Profile/Profile'
+import TicketPage from './components/TicketPage/TicketPage'
+import './App.css';
+import Admin from './components/Admin';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact render={props => <Homepage {...props} />} />
+          <Route path="/dashboard" exact render={props => <Admin {...props} />} />
+          <Route path="/login" render={props => <LogOrsign {...props} />} />
+          <Route path="/register" render={props => <Signup {...props} />} />
+          <Route path="/routes" exact render={props => <RouteSelection {...props} />} />
+          <Route path="/profile" exact render={props => <Profile {...props} />} />
+          <Route path="/getTicket" exact render={props => <TicketPage {...props} />} />
+        </Switch>
+      </Router>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb+srv://henda:belguesmi@cluster0.udfzt.mongodb.net/MERN?retryWrites=true&w=majority"
+}
